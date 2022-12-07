e9b1d834c043c9ea9051607c0bfd520e975085cd
Speedwag00n <INemankov@luxoft.com>
2022-12-07 18:47:36 +0100

    Add possibility to specify path to python37

--- scripts/do_sanity_check.bat
+++ scripts/do_sanity_check.bat
@@ -1,11 +1,12 @@
 set PATH=c:\python39\;c:\python39\scripts\;%PATH%
 set ENGINE=%1
-set PYTHON="C:\Python37\python.exe"
-set TOOL=%6
+set TOOL=%2
+set PYTHON=%3
 
 if not defined RETRIES set RETRIES=2
 if not defined UPDATE_REFS set UPDATE_REFS="No"
 if not defined TOOL set TOOL="..\USD\build\bin\usdview"
+if not defined PYTHON set PYTHON="C:\Python37\python.exe"
 
 python -m pip install -r ..\jobs_launcher\install\requirements.txt
 %PYTHON% -m pip install -r requirements.txt
--- scripts/run.bat
+++ scripts/run.bat
@@ -4,12 +4,13 @@ set TESTS_FILTER="%2"
 set ENGINE=%3
 set RETRIES=%4
 set UPDATE_REFS=%5
-set PYTHON="C:\Python37\python.exe"
 set TOOL=%6
+set PYTHON=%7
 
 if not defined RETRIES set RETRIES=2
 if not defined UPDATE_REFS set UPDATE_REFS="No"
 if not defined TOOL set TOOL="..\USD\build\bin\usdview"
+if not defined PYTHON set PYTHON="C:\Python37\python.exe"
 
 python -m pip install -r ..\jobs_launcher\install\requirements.txt
 %PYTHON% -m pip install -r requirements.txt

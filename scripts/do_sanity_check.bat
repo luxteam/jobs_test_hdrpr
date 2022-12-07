c783c0e493efc37e371ce88bf7fc55b3a96f6092
Speedwag00n <INemankov@luxoft.com>
2022-12-07 14:46:18 +0100

    Add possibility to set path to built USD

--- scripts/do_sanity_check.bat
+++ scripts/do_sanity_check.bat
@@ -1,10 +1,12 @@
 set PATH=c:\python39\;c:\python39\scripts\;%PATH%
 set ENGINE=%1
 set PYTHON="C:\Python37\python.exe"
+set TOOL=%6
 
 if not defined RETRIES set RETRIES=2
 if not defined UPDATE_REFS set UPDATE_REFS="No"
+if not defined TOOL set TOOL="..\USD\build\bin\usdview"
 
 python -m pip install -r ..\jobs_launcher\install\requirements.txt
 %PYTHON% -m pip install -r requirements.txt
-python ..\jobs\Scripts\sanityCheck.py --tool_path "..\USD\build\bin\usdview" --res_path "C:\TestResources\hdrpr_autotests_assets" --engine %ENGINE% --python %PYTHON%
+python ..\jobs\Scripts\sanityCheck.py --tool_path %TOOL% --res_path "C:\TestResources\hdrpr_autotests_assets" --engine %ENGINE% --python %PYTHON%
--- scripts/run.bat
+++ scripts/run.bat
@@ -5,10 +5,12 @@ set ENGINE=%3
 set RETRIES=%4
 set UPDATE_REFS=%5
 set PYTHON="C:\Python37\python.exe"
+set TOOL=%6
 
 if not defined RETRIES set RETRIES=2
 if not defined UPDATE_REFS set UPDATE_REFS="No"
+if not defined TOOL set TOOL="..\USD\build\bin\usdview"
 
 python -m pip install -r ..\jobs_launcher\install\requirements.txt
 %PYTHON% -m pip install -r requirements.txt
-python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir HdRPR --cmd_variables Python %PYTHON% Tool "..\USD\build\bin\usdview" ResPath "C:\TestResources\hdrpr_autotests_assets" Engine %ENGINE% Retries %RETRIES% UpdateRefs %UPDATE_REFS%
+python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir HdRPR --cmd_variables Python %PYTHON% Tool %TOOL% ResPath "C:\TestResources\hdrpr_autotests_assets" Engine %ENGINE% Retries %RETRIES% UpdateRefs %UPDATE_REFS%
